007465972dd866109e8157db31fbf44ab98ed46a
polaris525 <mekong331@gmail.com>
2021-05-26 04:18:41 +0800

    ERC721 & Crypto-Collectibles

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -1,7 +1,12 @@
 pragma solidity ^0.4.25;
 import "./Ownable.sol";
+import "./safemath.sol";
 
 contract ZombieFactory is Ownable{
+    using SafeMath for uint256;
+    using SafeMath32 for int32;
+    using SafeMath16 for int16;
+    
     event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
@@ -25,7 +30,7 @@ contract ZombieFactory is Ownable{
     function _createZombie(string _name, uint _dna) internal {
         uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
         zombieToOwner[id] = msg.sender;
-        ownerZombieCount[msg.sender]++;
+        ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].add(1);
         emit NewZombie(id, _name, _dna);
     }
 
