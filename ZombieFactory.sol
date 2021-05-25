2a8b79b25ac121112acbfc25ee39749191db23bc
polaris525 <mekong331@gmail.com>
2021-05-25 23:52:26 +0800

    zombie battle system

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.4.25;
-import "./ownable.sol";
+import "./Ownable.sol";
 
 contract ZombieFactory is Ownable{
     event NewZombie(uint zombieId, string name, uint dna);
@@ -13,6 +13,8 @@ contract ZombieFactory is Ownable{
         uint dna;
         uint32 level;
         uint32 readyTime;
+        uint16 winCount;
+        uint16 lossCount;
     }
 
     Zombie[] public zombies;
@@ -21,7 +23,7 @@ contract ZombieFactory is Ownable{
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
